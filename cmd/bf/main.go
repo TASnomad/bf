10ba28e981dfed95a87aa1731ca73a04eedf9f50
Martin Barreau <martin.barreau@gmail.com>
2021-11-14 18:13:44 +0100

    More command line arguments

--- cmd/bf/main.go
+++ cmd/bf/main.go
@@ -5,16 +5,46 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"time"
 
 	"github.com/TASnomad/bf/pkg/compiler"
 	"github.com/TASnomad/bf/pkg/vm"
 )
 
+var Build = "unknown"
+var Version = "unknown"
+
+func elapsed(what string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", what, time.Since(start))
+	}
+}
+
 func main() {
+
+	flag.Usage = func() {
+		usage := fmt.Sprintf("Usage: %s [option] [arg]\n\nBrainfuck VM\n\nOptions and arguments:", os.Args[0])
+		fmt.Fprint(flag.CommandLine.Output(), usage)
+		flag.PrintDefaults()
+	}
+
 	var file string
+	var showExecTime bool = false
 	flag.StringVar(&file, "f", "", "Brainfuck program file")
+	ver := flag.Bool("v", false, "Print the version number and exit")
+	perf := flag.Bool("perf", false, "Print VM execution time")
 
 	flag.Parse()
+
+	if *ver {
+		fmt.Printf("%s@%s", Version, Build)
+		os.Exit(0)
+	}
+
+	if *perf {
+		showExecTime = true
+	}
 
 	if len(file) == 0 {
 		fmt.Fprintf(os.Stderr, "Usage: %s:\n", os.Args[0])
@@ -29,5 +59,8 @@ func main() {
 	}
 	c := compiler.NewCompiler(string(code))
 	v := vm.NewVm(c.Compile(), os.Stdin, os.Stdout)
+	if showExecTime {
+		defer elapsed("bf VM execution")()
+	}
 	v.Execute()
 }
